42371e3227294b37e54b515fda1b2259963e862a
Dave-Palmer <djarmor@gmail.com>
2025-01-25 10:37:01 +0000

    add logger, thunk, and actions for robots fetch

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,20 @@ import React from "react";
 // import ReactDOM from 'react-dom'; The new way to import createRoot:
 import { createRoot } from "react-dom/client";
 import { Provider, connect } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { createLogger } from "redux-logger";
+import { thunk } from "redux-thunk";
 import "./index.css";
 import App from "./containers/App";
-import { searchRobots } from "./reducers";
+import { searchRobots, requestRobots } from "./reducers";
 import "tachyons";
 // import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(searchRobots);
+// creates a console log of each change in the state, includes the previous state, action, and next state
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 const root = createRoot(document.getElementById("root"));
 root.render(
